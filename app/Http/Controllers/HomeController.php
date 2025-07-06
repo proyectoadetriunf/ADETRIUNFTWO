a42c1154a0cfe32c61ad927fa3ec200f11f22a3e
Dani_42Enro@ <danielapadilla128@gmail.com>
2025-07-05 23:35:15 -0600

    Fix: eliminar Faker y usar coordenadas fijas por defecto

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,8 +10,6 @@ use App\Models\Persona;
 use App\Models\Solicitud;
 use App\Models\Recibo;
 
-use Faker\Factory as FakerFactory;
-
 class HomeController extends Controller
 {
     public function __construct()
@@ -20,57 +18,55 @@ class HomeController extends Controller
     }
 
     public function index()
-{
-    $totalProyectos = Proyecto::count();
-    $totalBeneficiarios = Persona::join('persona_roles', 'personas.persona_id', '=', 'persona_roles.persona_id')
-        ->where('persona_roles.rol_id', 3)
-        ->count();
-    $totalDonaciones = Recibo::count();
-    $totalSolicitudesPendientes = Solicitud::where('estado', 'pendiente')->count();
-    $proyectosSQL = Proyecto::orderBy('created_at', 'desc')->take(5)->get();
+    {
+        $totalProyectos = Proyecto::count();
+        $totalBeneficiarios = Persona::join('persona_roles', 'personas.persona_id', '=', 'persona_roles.persona_id')
+            ->where('persona_roles.rol_id', 3)
+            ->count();
+        $totalDonaciones = Recibo::count();
+        $totalSolicitudesPendientes = Solicitud::where('estado', 'pendiente')->count();
+        $proyectosSQL = Proyecto::orderBy('created_at', 'desc')->take(5)->get();
 
-    $beneficiarios = DB::connection('mongodb')
-        ->collection('beneficiarios')
-        ->count();
+        $beneficiarios = DB::connection('mongodb')
+            ->collection('beneficiarios')
+            ->count();
 
-    $proyectosRaw = DB::connection('mongodb')
-        ->collection('proyectos')
-        ->get();
-
-    $proyectos = collect($proyectosRaw)->map(function ($proyecto) {
-         $faker = FakerFactory::create();
-
-
-        $seguimientos = DB::connection('mongodb')
-            ->collection('seguimientos')
-            ->where('proyecto_id', $proyecto['_id'])
+        $proyectosRaw = DB::connection('mongodb')
+            ->collection('proyectos')
             ->get();
 
-        $totalAvance = 0;
-        foreach ($seguimientos as $s) {
-            $totalAvance += $s['avance'] ?? 0;
-        }
+        $proyectos = collect($proyectosRaw)->map(function ($proyecto) {
+            $seguimientos = DB::connection('mongodb')
+                ->collection('seguimientos')
+                ->where('proyecto_id', $proyecto['_id'])
+                ->get();
 
-        $proyecto['lat'] = $proyecto['lat'] ?? $faker->randomFloat(6, 13.095, 13.105);
-        $proyecto['lng'] = $proyecto['lng'] ?? $faker->randomFloat(6, -87.030, -87.020);
-        $proyecto['avance_total'] = $totalAvance;
+            $totalAvance = 0;
+            foreach ($seguimientos as $s) {
+                $totalAvance += $s['avance'] ?? 0;
+            }
 
-        return $proyecto;
-    });
+            // Coordenadas fijas por defecto si no existen
+            $proyecto['lat'] = $proyecto['lat'] ?? 13.1006;
+            $proyecto['lng'] = $proyecto['lng'] ?? -87.025;
+            $proyecto['avance_total'] = $totalAvance;
 
-    $proyectosActivos = $proyectos->filter(function ($proyecto) {
-        return !empty($proyecto['nombre']);
-    })->count();
+            return $proyecto;
+        });
 
-    return view('home', compact(
-        'totalProyectos',
-        'totalBeneficiarios',
-        'totalDonaciones',
-        'totalSolicitudesPendientes',
-        'proyectos',
-        'beneficiarios',
-        'proyectosActivos'
-    ));
+        $proyectosActivos = $proyectos->filter(function ($proyecto) {
+            return !empty($proyecto['nombre']);
+        })->count();
+
+        return view('home', compact(
+            'totalProyectos',
+            'totalBeneficiarios',
+            'totalDonaciones',
+            'totalSolicitudesPendientes',
+            'proyectos',
+            'beneficiarios',
+            'proyectosActivos'
+        ));
+    }
 }
 
-}
