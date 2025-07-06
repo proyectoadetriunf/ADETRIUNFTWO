01956e664206ff3ee924779411cc9acd039aa48c
Dani_42Enro@ <danielapadilla128@gmail.com>
2025-07-06 00:51:55 -0600

    Corrijo error de sintaxis y duplicaciones en HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,12 +28,7 @@ class HomeController extends Controller
         $totalSolicitudesPendientes = Solicitud::where('estado', 'pendiente')->count();
         $proyectosSQL = Proyecto::orderBy('created_at', 'desc')->take(5)->get();
 
-<<<<<<< HEAD
-        // 🔹 Datos desde MongoDB (como en ComuniController)
-        $beneficiarios = DB::connection('mongodb')
-            ->collection('beneficiarios')
-            ->count();
-=======
+        // 🔹 Datos desde MongoDB
         $beneficiarios = DB::connection('mongodb')
             ->collection('beneficiarios')
             ->count();
@@ -64,33 +59,6 @@ class HomeController extends Controller
         $proyectosActivos = $proyectos->filter(function ($proyecto) {
             return !empty($proyecto['nombre']);
         })->count();
->>>>>>> a42c1154a0cfe32c61ad927fa3ec200f11f22a3e
-
-        $proyectosRaw = DB::connection('mongodb')
-            ->collection('proyectos')
-            ->get();
-
-        $proyectos = collect($proyectosRaw)->map(function ($proyecto) {
-            $seguimientos = DB::connection('mongodb')
-                ->collection('seguimientos')
-                ->where('proyecto_id', $proyecto['_id'])
-                ->get();
-
-            $totalAvance = 0;
-            foreach ($seguimientos as $s) {
-                $totalAvance += $s['avance'] ?? 0;
-            }
-
-            $proyecto['lat'] = $proyecto['lat'] ?? fake()->randomFloat(6, 13.095, 13.105);
-            $proyecto['lng'] = $proyecto['lng'] ?? fake()->randomFloat(6, -87.030, -87.020);
-            $proyecto['avance_total'] = $totalAvance;
-
-            return $proyecto;
-        });
-
-        $proyectosActivos = $proyectos->filter(function ($proyecto) {
-            return !empty($proyecto['nombre']);
-        })->count();
 
         // 🔹 Enviar todo a la vista
         return view('home', compact(
@@ -103,9 +71,4 @@ class HomeController extends Controller
             'proyectosActivos'
         ));
     }
-<<<<<<< HEAD
 }
-=======
-}
-
->>>>>>> a42c1154a0cfe32c61ad927fa3ec200f11f22a3e
