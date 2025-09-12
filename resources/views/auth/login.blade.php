846c1842e008e1716102bde6bc52cf3aa3c38bfb
Dani_42Enro@ <danielapadilla128@gmail.com>
2025-09-12 15:28:46 -0600

    mejorado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<link rel="stylesheet" href="{{ asset('css/style.css') }}">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+
 
 <div class="d-flex align-items-center justify-content-center min-vh-100">
     <header class="main-header">
